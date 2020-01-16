c0c39950a71245b8413965634dd94f9bc5f064de
Unimusha <alextopota528@gmail.com>
2020-01-16 22:58:06 +0100

    Corrección en modelo Materia

--- app/Materia.php
+++ app/Materia.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Materia extends Model {
     protected $table = "materia";
 
-    public function materiaImpartida() {
+    public function materiasImpartidas() {
         return $this->hasMany('App\Materiaimpartida', "materia");
     }
-    public function materiaMatriculada() {
+    public function materiasMatriculadas() {
         return $this->hasMany('App\Materiamatriculada', "materia");
     }
 }
