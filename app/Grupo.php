d35efe22a84dcb33e82da1fd328a6882e7833c46
MVillaescusa <mariovillaescusa@hotmail.com>
2020-01-16 09:36:42 +0100

    Relacion mia hecha

--- app/Grupo.php
+++ app/Grupo.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Grupo extends Model
 {
-    //
+    /**
+     * Get the post that owns the comment.
+     */
+    public function anyoescolarObject()
+    {
+        return $this->belongsTo('App\Anyoescolar', 'anyoescolar');
+    }
 }
