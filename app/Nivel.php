3f3d4325b63316988549fd5dfd2fdfc0c76f8d60
HectorDawDosMares <48472352+HectorDawDosMares@users.noreply.github.com>
2020-01-30 10:15:17 +0100

    Update Nivel.php

--- app/Nivel.php
+++ app/Nivel.php
@@ -6,13 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nivel extends Model
 {
-
-    /*
-
-    -- One To Many con modelo Grupo F.K. nivel
-    -- One To Many con modelo Nivel F.K. nivelsuperior
-    -- One To Many (Inverse) con modelo Nivel F.K. nivelsuperior
-*/
     protected $table = 'niveles';
 
     protected $fillable = ['nombre','nivelsuperior'];
