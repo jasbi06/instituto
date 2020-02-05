7e21cd14c5baae8240082a216d62efc208d64a22
Marcfernic <1642969@alu.murciaeduca.es>
2020-02-05 12:02:20 +0000

    Relaciones y Model FaltaProfesor 05/02/2020

--- app/FaltaProfesor.php
+++ app/FaltaProfesor.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class FaltaProfesor extends Model
 {
     //
+    protected $fillable = ['profesor_falta', 'profesor_guardia', 'periodoclase_id'];
+    /**
+     * Get the periodos the time of the journey
+     */
+    public function periodosclases()
+    {
+        return $this->hasMany('App\Periodosclases', 'FaltaProfesor');
+    }
+
+    /**
+     * Get the coordinador who leave the class
+     */
+    public function profesorfaltaObject()
+    {
+        return $this->belongsTo('App\User', 'profesor_falta');
+    }
+
+    /**
+     * Get the professor who tke cares the class
+     */
+    public function profesorguardiaObjectObject()
+    {
+        return $this->belongsTo('App\User', 'profesor_guardia');
+    }
+
 }
