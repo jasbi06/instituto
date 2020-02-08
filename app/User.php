5f51599b0a2e58db501f616654817bcc4737bc91
HectorDawDosMares <7946424@alu.murciaeduca.es>
2020-02-08 11:23:20 +0100

    Issue #165 misGruposMatriculados() y misGruposImpartidos() en App\User.php

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable {
      *
      * @var array
      */
-    
+
     protected $fillable = [
         'name', 'email', 'password','provider','provider_id'
     ];
@@ -186,18 +186,16 @@ class User extends Authenticatable {
         return $booleano;
     }
 
-  
+
     public function misGruposMatriculados() {
-        return $this->hasManyThrough(
+        return $this->belongsToMany(
             'App\Grupo',
-            'App\Matricula',
-            'alumno', // Foreign key on anyosescolares table...
-            'id', // Foreign key on grupos table...
-            'id', // Local key on centros table...
-            'id' // Local key on anyosescolares table...
+            'materiasmatriculadas',
+            'alumno',
+            'grupo'
         );
     }
-  
+
     public function misProfesores(Nivel $nivel = null){
         //tenemos que sacar todas las matrículas que tiene un usuario
         $id = $this->id;
@@ -219,16 +217,14 @@ class User extends Authenticatable {
     }
 
     public function misGruposImpartidos() {
-        return $this->hasManyThrough(
-            'App\Grupo', //Destino
-            'App\Materiaimpartida', //Intermedio
-            'docente', // Foreign Key User > Materiaimpartida
-            'id', // Foreign Key Materiaimpartida > Grupo
-            'id', // Local Key User
-            'grupo' // Local Key Materiaimpartida
+        return $this->belongsToMany(
+            'App\Grupo',
+            'materiasimpartidas',
+            'docente',
+            'grupo'
         );
     }
-  
+
     public function misMateriasMatriculadas() {
         return $this->belongsToMany(
             'App\Materia',
